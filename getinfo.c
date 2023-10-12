df3c0a4f20fa0f6974240a130cc561a52db6af25
arnold-04 <arnoldkwame1@gmail.com>
2023-10-12 09:57:19 -0700

    code update for files in simple_shell repo

---
+++ getinfo.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ * Initialize the info_t structure.
+ * @param info: Address of the struct
+ */
+void initialize_info(info_t *info)
+{
+	info->arg = NULL;
+	info->argv = NULL;
+	info->path = NULL;
+	info->argc = 0;
+}
+
+/**
+ * Populate the info_t structure.
+ * @param info: Address of the struct
+ * @param av: Argument vector
+ */
+void populate_info(info_t *info, char **av)
+{
+	int i = 0;
+
+	info->fname = av[0];
+	if (info->arg)
+	{
+		info->argv = strtow(info->arg, " \t");
+		if (!info->argv)
+		{
+			info->argv = malloc(sizeof(char *) * 2);
+			if (info->argv)
+			{
+				info->argv[0] = _strdup(info->arg);
+				info->argv[1] = NULL;
+			}
+		}
+		for (i = 0; info->argv && info->argv[i]; i++)
+			;
+		info->argc = i;
+
+		replace_alias(info);
+		replace_vars(info);
+	}
+}
+
+/**
+ * Release memory associated with the info_t structure fields.
+ * @param info: Address of the struct
+ * @param all: True if freeing all fields
+ */
+void release_info(info_t *info, int all)
+{
+	ffree(info->argv);
+	info->argv = NULL;
+	info->path = NULL;
+	if (all)
+	{
+		if (!info->cmd_buf)
+			free(info->arg);
+		if (info->env)
+			free_list(&(info->env));
+		if (info->history)
+			free_list(&(info->history));
+		if (info->alias)
+			free_list(&(info->alias));
+		ffree(info->environ);
+		info->environ = NULL;
+		bfree((void **)info->cmd_buf);
+		if (info->readfd > 2)
+			close(info->readfd);
+		_putchar(BUF_FLUSH);
+	}
+}
+
